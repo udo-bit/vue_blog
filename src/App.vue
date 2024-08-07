1ca76c162838b142500218c6a71e00c29efcdc75
udo-bit <enote_gxl@163.com>
2024-08-07 22:49:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <Counter/>
+  <n-button>hello</n-button>
+  <n-card title="hello">
+    <p>hello</p>
+  </n-card>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
